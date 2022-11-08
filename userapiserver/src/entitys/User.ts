deea20862863164baa9a4782c1b005a62ac0809a
Tolga BAYRAK <tolga@pop-os.localdomain>
2022-11-08 15:55:16 +0300

    Entitiy validasyon işlemleri

--- userapiserver/src/entitys/User.ts
+++ userapiserver/src/entitys/User.ts
@@ -1,15 +1,27 @@
 import { Entity, Column, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm"
-
+import {
+    Length,
+    IsEmail,
+    IsFQDN,
+    Min,
+    Max,
+  } from 'class-validator';
 @Entity("users")
 export class User {
     @PrimaryGeneratedColumn("uuid")
     id!: number
 
+    @Min(3)
+    @Max(12)
     @Column()
     username!: string
 
+
     @Column()
+    @IsEmail()
+    @Length(6, 32)
     email!: string
+
 
     @Column()
     password!: string
