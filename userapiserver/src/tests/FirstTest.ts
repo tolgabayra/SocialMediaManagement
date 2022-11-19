89e54241ae9d9f6d85c2dccaf68999f4b3d0ecf8
Tolga BAYRAK <tolga@pop-os.localdomain>
2022-11-19 21:19:37 +0300

    Test

---
+++ userapiserver/src/tests/FirstTest.ts
@@ -0,0 +1,14 @@
+import app from "../app"
+import {chaiHttp} from "chai"
+import 'mocha';
+import { describe } from "mocha";
+import { expect } from "chai";
+
+
+chai.use(chaiHttp)
+
+describe("Test Config Here ... !", () => {
+  it("Pass", () => {
+    
+  })
+})
